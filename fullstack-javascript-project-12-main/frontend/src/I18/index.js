19006614140d7ade8428e128cb5f0fac7ac513df
ivanfrolovich <frolovitch.ivan@mail.ru>
2025-10-20 12:34:13 +0300

    fix: restore Vite setup with correct translations
    - Fix package.json to use Vite instead of react-scripts
    - Update translations to match test expectations exactly
    - Fix HTML structure for Vite
    - Remove conflicting dependencies
    - This should fix the registration test

---
+++ fullstack-javascript-project-12-main/frontend/src/I18/index.js
@@ -0,0 +1,5 @@
+import ru from './localization';
+
+export default {
+  ru,
+};
---
+++ fullstack-javascript-project-12-main/frontend/src/routes.js
@@ -0,0 +1,13 @@
+// @ts-check
+
+const apiPath = '/api/v1';
+
+const routes = {
+  loginPath: () => [apiPath, 'login'].join('/'),
+  signupPath: () => [apiPath, 'signup'].join('/'),
+  chatPagePath: () => '/',
+  loginPagePath: () => '/login',
+  signupPagePath: () => '/signup',
+};
+
+export default routes;
